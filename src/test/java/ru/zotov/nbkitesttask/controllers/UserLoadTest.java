30eaeb71bbba38acc0b3b8e666d9dfabbdcf5013
Ilya Zotov <ilya.zotov.01@mail.ru>
2024-10-06 21:33:12 +0300

    Unused imports

--- src/test/java/ru/zotov/nbkitesttask/controllers/UserLoadTest.java
+++ src/test/java/ru/zotov/nbkitesttask/controllers/UserLoadTest.java
@@ -11,10 +11,7 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
-import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
-import org.springframework.web.client.RestTemplate;
-import org.springframework.web.util.DefaultUriBuilderFactory;
 import ru.zotov.nbkitesttask.dto.UserRequest;
 import ru.zotov.nbkitesttask.dto.UserResponse;
 
