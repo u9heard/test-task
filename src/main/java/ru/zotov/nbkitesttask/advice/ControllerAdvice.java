5305e40847befcc826c675ae3b7a479f47a9323c
Ilya Zotov <ilya.zotov.01@mail.ru>
2024-10-07 09:47:23 +0300

    Use ProblemDetail in ControllerAdvice

--- src/main/java/ru/zotov/nbkitesttask/advice/ControllerAdvice.java
+++ src/main/java/ru/zotov/nbkitesttask/advice/ControllerAdvice.java
@@ -1,5 +1,9 @@
 package ru.zotov.nbkitesttask.advice;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ProblemDetail;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -8,15 +12,25 @@ import ru.zotov.nbkitesttask.exceptions.EmailAlreadyExistException;
 import ru.zotov.nbkitesttask.exceptions.UserNotFoundException;
 
 @RestControllerAdvice
+@Slf4j
 public class ControllerAdvice extends ResponseEntityExceptionHandler {
 
     @ExceptionHandler(value = {UserNotFoundException.class})
-    public ResponseEntity<Object> handleUserNotFoundException(UserNotFoundException ex) {
-        return ResponseEntity.notFound().build();
+    public ResponseEntity<ProblemDetail> handleUserNotFoundException(UserNotFoundException ex) {
+        ProblemDetail problemDetail = ProblemDetail.forStatusAndDetail(HttpStatus.NOT_FOUND, ex.getMessage());
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(problemDetail);
     }
 
     @ExceptionHandler(value = {EmailAlreadyExistException.class})
-    public ResponseEntity<Object> handleEmailAlreadyExistException(EmailAlreadyExistException ex) {
-        return ResponseEntity.badRequest().build();
+    public ResponseEntity<ProblemDetail> handleEmailAlreadyExistException(EmailAlreadyExistException ex) {
+        ProblemDetail problemDetail = ProblemDetail.forStatusAndDetail(HttpStatus.BAD_REQUEST, ex.getMessage());
+        return ResponseEntity.badRequest().body(problemDetail);
+    }
+
+    @ExceptionHandler(value = {Exception.class})
+    public ResponseEntity<ProblemDetail> handleException(Exception ex) {
+        log.error(ex.getMessage(), ex);
+        ProblemDetail problemDetail = ProblemDetail.forStatusAndDetail(HttpStatus.INTERNAL_SERVER_ERROR, "Something went wrong");
+        return ResponseEntity.internalServerError().body(problemDetail);
     }
 }
